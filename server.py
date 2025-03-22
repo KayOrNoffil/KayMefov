04b0fe81b4c5e1bf4e99bd4319f1684069c98dc0
Kay <168826722+KayOrNoffil@users.noreply.github.com>
2025-03-22 14:51:17 +0500

    11
    11

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask"
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
